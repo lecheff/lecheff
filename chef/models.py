c393364c511a5b89859c0c2b8c2cb095592d59a3
lecheff <Lecheff@outlook.com.br>
2017-12-14 19:27:22 -0300

    Alteração de max_length na models

--- chef/models.py
+++ chef/models.py
@@ -2,23 +2,23 @@ from django.db import models
 
 
 class Prato(models.Model):
-    nome = models.CharField(max_length=50, blank = False)
-    descricao_breve = models.CharField(max_length=50, blank = False, default='')
+    nome = models.CharField(max_length=200, blank = False)
+    descricao_breve = models.CharField(max_length=200, blank = False, default='')
 
     def __str__(self):
         return self.nome
 
 class Requisitado(models.Model):
     prato = models.ForeignKey(Prato, blank = False)
-    imagem = models.CharField(max_length=200, blank = False, default='')
+    imagem = models.CharField(max_length=1000, blank = False, default='')
     descricao = models.TextField(default='')
 
     def __str__(self):
         return str(self.prato)
 
 class Imagem(models.Model):
-    titulo = models.CharField(max_length=50, blank = True, null=True)
-    url = models.CharField(max_length=200, blank = False, default='')
+    titulo = models.CharField(max_length=200, blank = True, null=True)
+    url = models.CharField(max_length=1000, blank = False, default='')
 
     def __str__(self):
         return str(self.id)
